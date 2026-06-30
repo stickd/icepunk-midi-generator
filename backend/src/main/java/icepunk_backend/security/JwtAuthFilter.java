ac9c35c45d15578494b7e8b7e3a055593dabb945
Maksym Zaremskyy <maksym_zaremskyy@yahoo.com>
2026-06-30 08:59:07 +0200

    class import

--- backend/src/main/java/icepunk_backend/security/JwtAuthFilter.java
+++ backend/src/main/java/icepunk_backend/security/JwtAuthFilter.java
@@ -18,10 +18,10 @@ import java.util.Collections;
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
 
-    private final JwtService jwtService;
+    private final icepunk_backend.security.JwtService jwtService;
     private final UserRepository userRepository;
 
-    public JwtAuthFilter(JwtService jwtService, UserRepository userRepository) {
+    public JwtAuthFilter(icepunk_backend.security.JwtService jwtService, UserRepository userRepository) {
         this.jwtService = jwtService;
         this.userRepository = userRepository;
     }
